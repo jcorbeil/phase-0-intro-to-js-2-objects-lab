efa4ba25fb5ead78f3c2943a0dba111dfc28b86c
Jeremy Corbeil <jeremycorbeil9@yahoo.com>
2023-05-31 20:53:06 -0400

    finished intro-to-js-2-objects-lab

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Write your solution in this file!
+let employee = {
+    firstName: 'Jeremy',
+    lastName: 'Corbeil'
+};
+/*let address = {
+    street: '2730 sw 13th st',
+    city: 'Miami',
+    state: 'FL',
+    zipCode: '33145'
+}
+return.employee,addres*/
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+      ...employee,
+      [key]: value
+    };
+  }
+  
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+  
+  function deleteFromEmployeeByKey(employee, key) {
+    const updatedEmployee = { ...employee };
+    delete updatedEmployee[key];
+    return updatedEmployee;
+  }
+  
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
+  
